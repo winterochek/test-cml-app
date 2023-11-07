e83c5f03e533b282fdbf78a172584bc0873ae288
SergeyOcheretenko <ocheretenko.s@gmail.com>
2023-11-07 19:22:25 +0200

    increased zoom

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -72,7 +72,7 @@ export function Map() {
          <GoogleMap
             mapContainerStyle={containerStyle}
             center={center}
-            zoom={2}
+            zoom={8}
             onLoad={onLoad}
             onUnmount={onUnmount}
             onClick={onClick}
